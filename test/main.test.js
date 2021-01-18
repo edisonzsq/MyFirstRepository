6999d1d61b50b865c007ce66a1ed7e85441472ae
pigs-can-fly <edisen.zhuang@gmail.com>
2021-01-18 16:57:43 +0800

    test fail

--- test/main.test.js
+++ test/main.test.js
@@ -4,4 +4,8 @@ describe('Test', function(){
     it('should do the test', function(){
         assert.equal(1,1);
     })
+
+    it('should return error', function(){
+        assert.equal(1,2);
+    })
 });
